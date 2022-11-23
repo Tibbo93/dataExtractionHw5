8b6cad5bb2128d827f86d47bd5334f6227593b0f
Tibbo93 <alessandrotibaldi93@gmail.com>
2022-11-23 16:52:15 +0100

    Add value today company item

--- dataExtractionHw5/items.py
+++ dataExtractionHw5/items.py
@@ -1,12 +1,25 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class Dataextractionhw5Item(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ValueTodayCompanyItem(scrapy.Item):
+    id = scrapy.Field()
+    name = scrapy.Field()
+    headquarters_continent = scrapy.Field()
+    headquarters_country = scrapy.Field()
+    headquarters_sub_region = scrapy.Field()
+    headquarters_region = scrapy.Field()
+    annual_revenue_in_usd = scrapy.Field()
+    annual_net_income_in_usd = scrapy.Field()
+    annual_results_for_year_ending = scrapy.Field()
+    total_assets_in_usd = scrapy.Field()
+    total_liabilities_income_in_usd = scrapy.Field()
+    total_equity_in_usd = scrapy.Field()
+    company_business = scrapy.Field()
+    ipo_year = scrapy.Field()
+    ceo = scrapy.Field()
+    founders = scrapy.Field()
+    founded_year = scrapy.Field()
+    number_of_employees = scrapy.Field()
+    company_website = scrapy.Field()
+    world_rank = scrapy.Field()
+
