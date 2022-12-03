1d21a1d2be53d092ac1c050733a6445514c7bfb2
Paolo <tardioli.paolo@gmail.com>
2022-12-03 15:38:02 +0100

    Create Directory "datasets"

--- dataExtractionHw5/settings.py
+++ dataExtractionHw5/settings.py
@@ -14,9 +14,10 @@ NEWSPIDER_MODULE = 'dataExtractionHw5.spiders'
 # OUTPUT_FILE_PATHNAME = '/Users/alessandro/Desktop/Università/Primo anno/Primo semestre/Ingegneria dei dati ' \
 #            '22:23/Homework/Homework 5/value_today_companies_dataset.jsonl'
 
-# OUTPUT_FILE_PATHNAME = 'D:/Università/Ingegneria dei Dati/HOMEWORK/Homework 5/companies_marketcap_dataset.jsonl'
+FILENAME = 'disfold_dataset.jsonl'
+# FILENAME = 'companies_marketcap_dataset.jsonl'
 
-OUTPUT_FILE_PATHNAME = 'D:/Università/Ingegneria dei Dati/HOMEWORK/Homework 5/disfold_dataset.jsonl'
+OUTPUT_FILE_PATHNAME = './dataExtractionHw5/datasets/' + FILENAME
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 ' \
